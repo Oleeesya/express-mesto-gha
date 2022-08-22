1abba8c05ae0f457f236853a6c24cb59e68726da
Olesya <apple@MacBook-Air-Apple.local>
2022-08-22 16:58:35 +0300

    fix required fileds in joi

--- app.js
+++ app.js
@@ -21,12 +21,12 @@ app.post('/signin', login);
 
 app.post('/signup', celebrate({
   body: Joi.object().keys({
-    name: Joi.string().required().min(2).max(30),
-    about: Joi.string().required().min(2).max(30),
-    avatar: Joi.string().regex(/[-a-zA-Z0-9@:%._~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_.~#?&//=]*)?/i).required(),
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string().regex(/[-a-zA-Z0-9@:%._~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_.~#?&//=]*)?/i),
     email: Joi.string().required().email(),
     password: Joi.string().required().min(8),
-  }),
+  }).unknown(true),
 }), createUser);
 
 app.get('/me', auth, getCurrebtUserInfo);
